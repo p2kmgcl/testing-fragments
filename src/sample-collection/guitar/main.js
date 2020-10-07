abb222675ad66f4009ea052275864b3abc252573
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-10-07 13:08:38 +0200

    Playing sound (press P when a string is focused)

--- src/sample-collection/guitar/main.js
+++ src/sample-collection/guitar/main.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
 import Guitar, { getRenderFingerSpn } from "react-guitar";
 import { standard } from "react-guitar-tunings";
+import useSound from 'react-guitar-sound';
+
+const samples = ['E2', 'D3', 'G3', 'E4'].reduce(
+  (samples, note) => Object.assign(samples, {
+    [note]: `https://unpkg.com/react-guitar-sound@0.11.1/resources/${note}.mp3`
+  }),
+  {}
+)
 
 export default function () {
   const [strings, setStrings] = useState([0, 1, 2, 2, 0, -1])
+  const {play} = useSound(samples, strings, standard)
   return (
     <Guitar
       strings={strings}
       renderFinger={getRenderFingerSpn(standard)}
       onChange={setStrings}
+      onPlay={play}
     />
   );
 }
