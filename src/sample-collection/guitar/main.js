eb87bfaef6ce686458f9a54f214b395ab72636c8
Alejandro Tardín <alejandro.tardin@liferay.com>
2020-10-07 12:57:02 +0200

    Displaying note names from a standard guitar

--- src/sample-collection/guitar/main.js
+++ src/sample-collection/guitar/main.js
@@ -1,8 +1,12 @@
 import React from "react";
-import Guitar from "react-guitar";
+import Guitar, { getRenderFingerSpn } from "react-guitar";
+import { standard } from "react-guitar-tunings";
 
 export default function () {
   return (
-    <Guitar strings={[0, 1, 2, 2, 0, -1]} />
+    <Guitar
+      strings={[0, 1, 2, 2, 0, -1]}
+      renderFinger={getRenderFingerSpn(standard)}
+    />
   );
 }
