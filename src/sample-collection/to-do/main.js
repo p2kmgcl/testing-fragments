b5ec2a03a9c4d411f93f6a2a7a0d68cfe99ba55e
Pablo Molina <contact@pablomolina.me>
2020-10-07 12:12:40 +0200

    Add ToDo fragment

---
+++ src/sample-collection/to-do/main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { TodoApp } from "./src/TodoApp";
+
+export default function () {
+  return <TodoApp />;
+}
---
+++ src/sample-collection/to-do/src/TodoApp.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { TodoList } from "./TodoList";
+
+export class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">What needs to be done?</label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>Add #{this.state.items.length + 1}</button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ""
+    }));
+  }
+}
