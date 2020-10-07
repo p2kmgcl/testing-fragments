bb98459599ad5f8e3fdd2de9be9d1e374749ccd2
Pablo Molina <contact@pablomolina.me>
2020-10-07 12:06:17 +0200

    Set guitar max width

--- src/sample-collection/guitar/main.js
+++ src/sample-collection/guitar/main.js
@@ -3,7 +3,7 @@ import Guitar from "react-guitar";
 
 export default function () {
   return (
-    <div style={{ width: "100%", overflowX: "auto" }}>
+    <div style={{ width: "50vw", overflowX: "auto" }}>
       <Guitar strings={[0, 1, 2, 2, 0, -1]} />
     </div>
   );
