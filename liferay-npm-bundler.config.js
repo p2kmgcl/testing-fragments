131e24742a29137e13f69d68c3af1d470e79671a
Pablo Molina <contact@pablomolina.me>
2020-10-30 08:33:47 +0100

    Support SCSS in bundler

--- liferay-npm-bundler.config.js
+++ liferay-npm-bundler.config.js
@@ -1,1 +1,23 @@
-module.exports = require('generator-liferay-fragments').getBundlerConfig();
+const config = require('generator-liferay-fragments').getBundlerConfig();
+
+module.exports = {
+  ...config,
+
+  webpack: {
+    ...config.webpack,
+    module: {
+      ...config.webpack.module,
+      rules: [
+        ...config.webpack.module.rules,
+        {
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ],
+          test: /\.s[ac]ss$/i,
+        }
+      ]
+    }
+  }
+};
